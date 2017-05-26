845c296f27275cb9b084e814ea09cfda33d7ac90
Hasan-git <hasan.ri25@gmail.com>
2017-05-27 01:02:12 +0300

    push changes

--- cms/js/common/common.services.js
+++ cms/js/common/common.services.js
@@ -6,8 +6,8 @@
         .module("common.services",["ngResource"])
     	.constant("appSettings",
         {
-            // serverPath: "http://192.168.1.110:63392"
-            // serverPath: "http://localhost:63392"
-            serverPath: "http://haisam:63392"
+            serverPath: "http://192.168.1.110:63392"
+            //serverPath: "http://localhost:63392"
+            //serverPath: "http://haisam:63392"
         });
 }());
